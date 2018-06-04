741638b7816af0b06431a16ac489c7037af82036
Guilherme Santos <guisperandio89@gmail.com>
2018-06-04 16:25:18 +0100

    pwa 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Progressive Web cat';
+  title = 'Progressive Web Cat ';
 }
